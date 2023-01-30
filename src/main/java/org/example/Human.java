303decc5fdca075f2ba1729c2ffb7591d1c0fdad
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-01-30 18:10:43 +0530

    add the description to the methods in the interfaces

--- src/main/java/org/example/Human.java
+++ src/main/java/org/example/Human.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Component;
 
 @Component
 public interface Human {
-    void name();
-    void profession();
-    void age();
-   void Gender();
-   void motherTongeu();
+    void name();//method that returns the name of the object
+    void profession();//method that returns the profession of the object is called
+    void age();//that returns the age
+   void Gender();//returns the gender of the object
+   void motherTongeu(); // method that print the mother tongue
 
 }
