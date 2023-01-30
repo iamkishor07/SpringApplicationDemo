c5c39917c07be915a832faadf9c878f3487d7c81
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-01-30 10:43:32 +0530

    intial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         ApplicationContext factory=new AnnotationConfigApplicationContext(AppConfig.class);
     LivingThing obj=  factory.getBean(LivingThing.class);
-    obj.display();
+        obj.display();
 
     }
 }
