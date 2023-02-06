52cfc604e9eb52923487543236d01d7ec07fbcad
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-02-06 16:18:36 +0530

    made some changes

--- src/main/java/org/example/AppConfig.java
+++ src/main/java/org/example/AppConfig.java
@@ -5,7 +5,17 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = "org.example")
+@ComponentScan(basePackages = "org.ComponentScan")
+//It is used to specify the path to the container in which it needs to search for the beans or components
+//in the specified directory
+//if it is not set properly it may throws bean not found exception
+
+//While using the @SpringBootApplication annotation, it takes the default path/directory of that class
+//for searching beans in that specified path
+//if at all we need to change thepath that is been taken by the Default SpringBOotAPplication annotaion
+//we use ComponentScan to again specify subpackages or any other  path/Directory to the container to search for the beans
+
 public class AppConfig {
+
 
 }
--- src/main/java/org/example/SpringBasicApplicationDemo.java
+++ src/main/java/org/example/SpringBasicApplicationDemo.java
@@ -1,10 +1,11 @@
 package org.example;
 
 
+import org.example.Basic.LivingThing;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Main {
+public class SpringBasicApplicationDemo {
     public static void main(String[] args) {
         ApplicationContext factory=new AnnotationConfigApplicationContext(AppConfig.class);
 
