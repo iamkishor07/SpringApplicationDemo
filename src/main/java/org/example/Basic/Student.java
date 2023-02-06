f68fb9f3bdb06fa38ea16b6d2d7156191239017d
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-02-06 16:18:23 +0530

    changed directory

--- src/main/java/org/example/Basic/Student.java
+++ src/main/java/org/example/Basic/Student.java
@@ -1,8 +1,6 @@
-package org.example;
+package org.example.Basic;
 
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
