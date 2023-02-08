8434cff3ae0c4ff5121dcc1e0ef5fafca58ab768
Kishorkumar Dopathi <kishorkumardopathi16@gmail.com>
2023-02-08 10:45:48 +0530

    used replaced appconfig with configuration annotation and componentScan for the path/package search by the container for beans

--- src/main/java/org/Constructor/SpringPostandPreConstructorDemo.java
+++ src/main/java/org/Constructor/SpringPostandPreConstructorDemo.java
@@ -1,14 +1,15 @@
-package org.example;
+package org.Constructor;
 
-
-import org.Constructor.PostConstructor_PreDestroyDemo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan
 public class SpringPostandPreConstructorDemo {
     public static void main(String[] args) throws Exception{
-        ApplicationContext factory=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext factory=new AnnotationConfigApplicationContext(SpringPostandPreConstructorDemo.class);
 //      ApplicationContext factory=SpringApplication.run(SpringScopeApplicationDemo.class, args);
 
 
